5301692a98414ead4fb3295e7230eba4dc027202
Xumj82 <xumj1996@gmail.com>
2022-05-23 14:08:10 +0800

    update

--- data/patch_data.py
+++ data/patch_data.py
@@ -6,7 +6,7 @@ import os.path as op
 import numpy as np
 import pandas as pd
 import torch
-import elasticdeform
+import albumentations as A
 from torchvision import transforms
 
 import torch.utils.data as data
@@ -21,6 +21,7 @@ class PatchData(data.Dataset):
                 class_num = 5,
                 train=True,
                 repeat_channel = False,
+                elastic_param = 300,
                 aug_prob=0.5,
                 img_mean=(0.485),
                 img_std=(0.229)):
@@ -31,6 +32,7 @@ class PatchData(data.Dataset):
         self.data_dir = data_dir
         self.stage = stage
         self.aug_prob = aug_prob
+        self.elastic_param = elastic_param
         self.class_num = class_num
         self.repeat_channel = repeat_channel
         if self.stage=='fit':
@@ -40,6 +42,22 @@ class PatchData(data.Dataset):
 
         self.check_files()
 
+    @staticmethod
+    def train_test_split_on_patient(data_list ,test_size, random_state):
+        patients = []
+        for index, row in data_list.iterrows():
+            patientid = row['img_id'].split('_')[1] + row['img_id'].split('_')[2]
+            patients.append(patientid)
+        data_list['PatientID'] = patients
+
+        patientdict = list( dict.fromkeys(patients) )
+        traindict, testdict = train_test_split(patientdict, test_size=test_size, random_state=random_state)
+        traindict = pd.DataFrame(data=traindict, columns=["PatientID"])
+        testdict = pd.DataFrame(data=testdict, columns=["PatientID"])
+        data_train = pd.merge(traindict, data_list,  on ='PatientID', how ='left')
+        data_test = pd.merge(testdict, data_list,  on ='PatientID', how ='left')
+        return data_train,data_test
+
     def check_files(self):
         # This part is the core code block for load your own dataset.
         # You can choose to scan a folder, or load a file list pickle
@@ -48,7 +66,7 @@ class PatchData(data.Dataset):
         file_list_path = op.join(self.data_dir, self.csv_file)
         df = pd.read_csv(file_list_path)
         if self.stage=='fit':
-            fl_train, fl_val = train_test_split(df, test_size=0.1, random_state=32)
+            fl_train, fl_val = self.train_test_split_on_patient(df, test_size=0.1, random_state=32)
             self.data_list = fl_train if self.train else fl_val
         else:
             self.data_list = df
@@ -69,10 +87,9 @@ class PatchData(data.Dataset):
         else:
             img = cv2.imread(os.path.join(self.data_dir,'img_dir/test',self.data_list.iloc[idx]['patch_id']+'.png'),cv2.IMREAD_ANYDEPTH)
 
-        if self.train:
-            img = elasticdeform.deform_random_grid(img,sigma=15, points=3,axis=(0,1))
+        # if self.train:
+        #     img = elasticdeform.deform_random_grid(img,sigma=15, points=3,axis=(0,1))
 
-        img = torch.unsqueeze(torch.from_numpy(img/65536), 0).float()
 
         if self.class_num == 3:
             if str(self.data_list.iloc[idx]['pathology']) == 'NORMAL':
@@ -90,16 +107,20 @@ class PatchData(data.Dataset):
             else:
                 label = 1 if str(self.data_list.iloc[idx]['type']) == 'calcification' else 4
                 
-        trans = torch.nn.Sequential(
-            transforms.RandomHorizontalFlip(self.aug_prob),
-            transforms.RandomVerticalFlip(self.aug_prob),
-            transforms.RandomRotation(10),
-            transforms.Normalize(self.img_mean, self.img_std)
-        ) if self.train else torch.nn.Sequential(
-            transforms.Normalize(self.img_mean, self.img_std)
-        )
+        aug = A.Compose([
+            A.ToFloat(max_value=65535.0),
+            A.Normalize(mean=self.img_mean, std=self.img_std),
+            A.VerticalFlip(p=self.aug_prob),              
+            A.RandomRotate90(p=self.aug_prob),
+            A.ElasticTransform(alpha=self.elastic_param, sigma=self.elastic_param * 0.05, alpha_affine=self.elastic_param * 0.03, p=self.aug_prob),    
+            ]
+        ) if self.train else A.Compose([
+            A.ToFloat(max_value=65535.0),
+            A.Normalize(mean=self.img_mean, std=self.img_std),
+        ])
 
-        img_tensor = trans(img)
+        img_auged = aug(image=img)["image"]
+        img_tensor = torch.tensor(img_auged)
         if self.repeat_channel:
             img_tensor = img_tensor.repeat(3,1,1)
         lesion_tensor = torch.tensor(label)
